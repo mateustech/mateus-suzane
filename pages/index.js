196f3884da43d9ba0ac64eed156190130647aeb6
Mateus Fernandes Costa <mateustech686@gmail.com>
2021-05-10 21:20:25 -0300

    first page

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Mateus e Suzane</title>
         <meta name="description" content="Aceita ser nossos padrinhos?" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Aceita ser nossos<a href="">Padrinhos?</a>
+      <main className="flex flex-col w-screen h-screen items-center justify-center">
+        <h1 className="font-bold text-gray-800 ml-10"> Aceita ser nossos</h1>
+        <h1 className="font-primary text-primary text-6xl">
+         
+         Padrinhos?
         </h1>
-
-      
       </main>
-
-     
     </div>
-  )
+  );
 }

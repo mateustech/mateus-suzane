0704d4c316784e3395be9e8b9e89cef869764983
Mateus Fernandes Costa <mateustech686@gmail.com>
2021-05-14 22:19:05 -0300

    create layout page pallet

--- pages/step5.js
+++ pages/step5.js
@@ -66,58 +66,43 @@ function SwipeableTextMobileStepper() {
         />
       </Head>
 
-      <main className="flex flex-col w-screen h-screen items-center justify-between py-10">
-        <div className="flex flex-col items-center justify-center">
-          <main className="flex flex-col  items-center justify-center">
-            <h1 className="text-gray-700 font-bold text-2xl">
-              Manual Dos Padrinhos
-            </h1>
-            <h1 className="font-bold text-xl text-primary">
-              Paleta de Cores
-            </h1>
-          </main>
-          <div className="flex flex-col w-full mt-4 items-center justify-end ">
-            
-            <ul className="flex items-center justify-center mt-4">
-              {["#ab4a5d", "#8d334d", "#921a3e", "#6f1d34", "#60001c"].map(
-                (item) => {
-                  return (
-                    <li
-                      style={{ backgroundColor: item }}
-                      className="w-14 h-14 rounded-full m-1"
-                    ></li>
-                  );
-                }
-              )}
-            </ul>
-            <ul className="flex items-center justify-center">
-              {["#ab4a5d", "#8d334d", "#921a3e", "#6f1d34", "#60001c"].map(
-                (item) => {
-                  return (
-                    <li
-                      style={{ backgroundColor: item }}
-                      className="w-14 h-20 rounded m-1"
-                    ></li>
-                  );
-                }
-              )}
-            </ul>
-            <aside className="px-5">
-              <img className="w-full rounded mt-4" src="recorte.jpeg" alt="" />
-            </aside>
-          </div>
-          <div className="flex w-full items-center justify-between px-10 py-4 pb-20">
-            <Button size="small" onClick={handleBack}>
-              <KeyboardArrowLeft />
-              Voltar
-            </Button>
-            <Button size="small" onClick={handleNext}>
-              Próximo
-              <KeyboardArrowRight />
-            </Button>
+      <main className="flex flex-col px-4  w-screen h-screen items-center justify-center py-10">
+        <h1 className="text-primary font-primary text-4xl">Querida Madrinha</h1>
+        <h1 className="text-center mt-4  text-gray-700">
+          Escolhemos a cor <b className="text-primary italic">vinho</b> com
+          muito carinho para o nosso grande dia, e temos certeza de que você
+          ficará linda usando um vestido longo em um dos seguintes tons.
+        </h1>
+        <div className="flex flex-col w-full relative items-center justify-start ">
+          <ul className="flex w-full items-center justify-center mt-4">
+            {["#6f1d34", "#921a3e", "#6f1d34", "#60001c"].map((item) => {
+              return (
+                <li
+                  style={{ backgroundColor: item }}
+                  className="w-14 h-14 rounded-full m-1"
+                ></li>
+              );
+            })}
+          </ul>
+          <div className="flex w-full mt-4 items-start justify-evenly">
+          <h1 className="font-primary text-4xl">Inspire-se</h1>
+            <img
+              className="w-32 h-32 rounded-full mt-4"
+              src="recorte.jpeg"
+              alt=""
+            />
           </div>
         </div>
-        <div className="flex w-full items-center justify-between px-10 py-4"></div>
+        <div className="flex w-full items-center justify-between mt-10 px-10 py-4">
+          <Button size="small" onClick={handleBack}>
+            <KeyboardArrowLeft />
+            Voltar
+          </Button>
+          <Button size="small" onClick={handleNext}>
+            Próximo
+            <KeyboardArrowRight />
+          </Button>
+        </div>
       </main>
     </div>
   );
